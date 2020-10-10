4c63f9c40dffe04e797a405db8fbfaaeed70f3c6
Din Sebastian <dinsebi22@gmail.com>
2020-10-10 13:02:59 +0300

    Add files via upload
    Simple 2D Vector implementation by Me

---
+++ Vector.js
@@ -0,0 +1,76 @@
+//////////////////////////////////////////////////////////////////////////////////
+
+class Vector {
+  constructor(x, y) {
+    this.x = x;
+    this.y = y;
+  }
+
+  add(otherVector) {
+    this.x = this.x + otherVector.x;
+    this.y = this.y + otherVector.y;
+    return this;
+  }
+
+  subtract(otherVector) {
+    this.x = this.x - otherVector.x;
+    this.y = this.y - otherVector.y;
+    return this;
+  }
+
+  divideBy(scalar) {
+    this.x = this.x / scalar;
+    this.y = this.y / scalar;
+    return this;
+  }
+
+  distance(otherVector) {
+    let copy = new Vector(this.x, this.y);
+    copy.subtract(otherVector);
+    return copy.magnitude();
+  }
+
+  multiplyBy(scalar) {
+    this.x = this.x * scalar;
+    this.y = this.y * scalar;
+    return this;
+  }
+
+  //   negate() {
+  //     this.x = -this.x;
+  //     this.y = -this.y;
+  //     return this;
+  // }
+
+  magnitudeSquared() {
+    return this.x * this.x + this.y * this.y;
+  }
+
+  magnitude() {
+    return Math.sqrt(this.magnitudeSquared());
+  }
+
+  setMagnitude(multiplier) {
+    this.normalize();
+    this.multiplyBy(multiplier);
+    return this;
+  }
+
+  normalize() {
+    var magnitude = this.magnitude();
+    if (magnitude !== 0) {
+      this.multiplyBy(1 / magnitude);
+    }
+    return this;
+  }
+
+  limit(value) {
+    let magnitudeSq = this.magnitudeSquared();
+    if (magnitudeSq > value * value) {
+      this.divideBy(Math.sqrt(magnitudeSq));
+      this.multiplyBy(value);
+    }
+  }
+}
+
+////////////////////////////////////////////////////////////////////////////////
